d46895ea903a6d073f19c1ae571f3e9ca5dbf362
Magisquare <magisquare@163.com>
2015-04-17 12:38:43 +0800

    April 17th Fri. 	new file:   HelloWorld.java 	new file:   VariableName.java

---
+++ VariableName.java
@@ -0,0 +1,8 @@
+public class VariableName{
+	public static void main(String[] args) {
+		String name = "Ako";
+		System.out.println("My name is "+name);
+		name = "Bko";
+		System.out.println("Now, I changed my name to "+name);
+	}
+}
